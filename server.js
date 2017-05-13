af98a2ce7d57f4d9fe1dd4aa1db10a5e8b14c878
radedoc <radedoc@yahoo.com>
2017-05-13 03:34:52 -0400

    Added Buttons and Allowed Alexa strings

--- server.js
+++ server.js
@@ -342,7 +342,7 @@ function createAccount(email, password, passwordConf, username, callbackSucc, ca
 
       var userDB = db.collection('users')
       //CHECK IF DB CONTAINS ACCOUNT WITH THAT EMAIL BEFORE CREATING NEW ACCOUNT
-      userDB.find({'username' : username}).toArray(function(err, result) {
+      userDB.find({'username' : username}, {'email' : email}).toArray(function(err, result) {
         if (err) {
             console.log(err);
         } else if (result.length) {
